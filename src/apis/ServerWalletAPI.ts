fe7ba6824847df06de3e77bef9b499fcd03d3e5c
xiaomeng xu <a3824036@126.com>
2021-11-22 10:24:21 +0800

    update.

--- src/apis/ServerWalletAPI.ts
+++ src/apis/ServerWalletAPI.ts
@@ -2,6 +2,15 @@ import { SERVER_URL } from '../constants'
 import axios, { AxiosInstance } from 'axios'
 import mockProjectImg from '../assets/img/icon_mock_project.svg'
 
+export interface JsonResponseData<T = any> {
+  success: boolean
+  data?: T
+  error?: {
+    message: string
+    code: string | number
+  }
+}
+
 export interface projectData {
   name: string
   link: string
@@ -15,11 +24,25 @@ export interface projectGroupData {
 
 export type portfolioData = projectGroupData[]
 
+export interface reportData {
+  id: number
+  url: string
+  title: string
+  displayDatetime: Date
+}
+
 export class ServerWalletAPI {
   private readonly axios: AxiosInstance
 
   constructor() {
     this.axios = axios.create({ baseURL: SERVER_URL })
+  }
+
+  private getResponseData<T>(data: JsonResponseData<T>): T | undefined | null {
+    if (data.success) {
+      return data.data
+    }
+    return null
   }
 
   // TODO:
@@ -54,6 +77,13 @@ export class ServerWalletAPI {
     })
 
     return await Promise.resolve(mock)
+  }
+
+  async getReports(): Promise<reportData[]> {
+    const resp = await this.axios.get('/api/v1/reports')
+    const data = this.getResponseData<reportData[]>(resp.data)
+    if (data) return data
+    throw new Error('fetch error')
   }
 }
 
--- src/views/Home/Reports/index.tsx
+++ src/views/Home/Reports/index.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import styled from 'styled-components'
 import { useTranslation } from 'react-i18next'
 import HomeContentBlock from '../../../components/HomeContentBlock'
 import HomeContentTitle from '../../../components/HomeContentTitle'
 import { HomeActiveMenu } from '../../../store/system'
+import serverWalletAPI, { reportData } from '../../../apis/ServerWalletAPI'
 
 export const Reports: React.FC = () => {
   const { t } = useTranslation('trans')
+  const [data, setData] = useState<reportData[]>([])
+
+  useEffect(() => {
+    serverWalletAPI
+      .getReports()
+      .then(setData)
+      .catch((e) => console.log(e))
+  }, [])
+
+  console.log(data)
+
   return (
     <HomeContentBlock activeTag={HomeActiveMenu.reports}>
       <div id="reports" className="single-page">
         <HomeContentTitle.T1>{t('sider.menu.reports')}</HomeContentTitle.T1>
+        {data.map((report) => (
+          <HomeContentTitle.T2 key={report.id}>
+            <a href={report.url} target="_blank" className="report-link">
+              {report.title}
+            </a>
+          </HomeContentTitle.T2>
+        ))}
       </div>
     </HomeContentBlock>
   )
